4637d25b3d0c827f43f975ee4ac59dca68c022d3
Guilherme Totti <dev.guilhermetotti@gmail.com>
2024-01-31 13:15:42 -0300

    🔧 refactor: apply dependency inversion principle to user register service

--- src/services/register.ts
+++ src/services/register.ts
@@ -1,34 +1,28 @@
 import { hash } from 'bcryptjs'
-import { prisma } from '@/lib/prisma'
+import { UsersRepository } from '@/repositories/users-repository'
 
-interface registerServiceRequest {
+interface RegisterServiceRequest {
   name: string
   email: string
   password: string
 }
 
-export async function registerService({
-  name,
-  email,
-  password,
-}: registerServiceRequest) {
-  const password_hash = await hash(password, 6)
+export class RegisterService {
+  constructor(private usersRepository: UsersRepository) {}
 
-  const userWithSameEmail = await prisma.user.findUnique({
-    where: {
-      email,
-    },
-  })
+  async execute({ name, email, password }: RegisterServiceRequest) {
+    const password_hash = await hash(password, 6)
 
-  if (userWithSameEmail) {
-    throw new Error('E-mail already exists.')
-  }
+    const userWithSameEmail = await this.usersRepository.findByEmail(email)
 
-  await prisma.user.create({
-    data: {
+    if (userWithSameEmail) {
+      throw new Error('E-mail already exists.')
+    }
+
+    await this.usersRepository.create({
       name,
       email,
       password_hash,
-    },
-  })
+    })
+  }
 }
