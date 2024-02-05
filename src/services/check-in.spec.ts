7ce686f00b907bce6d60288eaf02d0ff50f01ebe
Guilherme Totti <dev.guilhermetotti@gmail.com>
2024-02-05 08:54:20 -0300

    ✨ feat: add check-in service

---
+++ src/services/check-in.spec.ts
@@ -0,0 +1,22 @@
+import { expect, describe, it, beforeEach } from 'vitest'
+import { InMemoryCheckInsRepository } from '@/repositories/in-memory/in-memory-check-ins-repository'
+import { CheckInService } from './check-in'
+
+let checkInsRepository: InMemoryCheckInsRepository
+let sut: CheckInService
+
+describe('Authenticate Service', () => {
+  beforeEach(() => {
+    checkInsRepository = new InMemoryCheckInsRepository()
+    sut = new CheckInService(checkInsRepository)
+  })
+
+  it('should be able to check in', async () => {
+    const { checkIn } = await sut.execute({
+      gymId: 'gym-01',
+      userId: 'user-01',
+    })
+
+    expect(checkIn.id).toEqual(expect.any(String))
+  })
+})
