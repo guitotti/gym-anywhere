d7a7eda37aae40fb056faef08af32112b384fa4b
Guilherme Totti <dev.guilhermetotti@gmail.com>
2024-02-07 15:54:51 -0300

    ✨ feat: add fetch user check-in history service

---
+++ src/services/fetch-user-check-ins-history.ts
@@ -0,0 +1,29 @@
+import { CheckInsRepository } from '@/repositories/check-ins-repository'
+import { CheckIn } from '@prisma/client'
+
+interface FetchUserCheckInsHistoryServiceRequest {
+  userId: string
+  page: number
+}
+
+interface FetchUserCheckInsHistoryServiceResponse {
+  checkIns: CheckIn[]
+}
+
+export class FetchUserCheckInsHistoryService {
+  constructor(private checkInsRepository: CheckInsRepository) {}
+
+  async execute({
+    userId,
+    page,
+  }: FetchUserCheckInsHistoryServiceRequest): Promise<FetchUserCheckInsHistoryServiceResponse> {
+    const checkIns = await this.checkInsRepository.findManyByUserId(
+      userId,
+      page,
+    )
+
+    return {
+      checkIns,
+    }
+  }
+}
