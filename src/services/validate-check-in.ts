61e1ef1c168903e21e2b45e1598ef81902beea8c
Guilherme Totti <dev.guilhermetotti@gmail.com>
2024-02-09 18:55:50 -0300

    ✨ feat: implements validate check-in service

---
+++ src/services/validate-check-in.ts
@@ -0,0 +1,44 @@
+import { CheckInsRepository } from '@/repositories/check-ins-repository'
+import { CheckIn } from '@prisma/client'
+import { ResourceNotFoundError } from './errors/resource-not-found-error'
+import dayjs from 'dayjs'
+import { LateCheckInValidationError } from './errors/late-check-in-validation-error'
+
+interface ValidateCheckInServiceRequest {
+  checkInId: string
+}
+
+interface ValidateCheckInServiceResponse {
+  checkIn: CheckIn
+}
+
+export class ValidateCheckInService {
+  constructor(private checkInsRepository: CheckInsRepository) {}
+
+  async execute({
+    checkInId,
+  }: ValidateCheckInServiceRequest): Promise<ValidateCheckInServiceResponse> {
+    const checkIn = await this.checkInsRepository.findById(checkInId)
+
+    if (!checkIn) {
+      throw new ResourceNotFoundError()
+    }
+
+    const intervalInMinutesFromCheckInCreation = dayjs(new Date()).diff(
+      checkIn.created_at,
+      'minutes',
+    )
+
+    if (intervalInMinutesFromCheckInCreation > 20) {
+      throw new LateCheckInValidationError()
+    }
+
+    checkIn.validated_at = new Date()
+
+    await this.checkInsRepository.save(checkIn)
+
+    return {
+      checkIn,
+    }
+  }
+}
