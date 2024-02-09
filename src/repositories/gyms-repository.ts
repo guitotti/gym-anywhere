351605ba294937fed25d0f4e159e4051bba0d67a
Guilherme Totti <dev.guilhermetotti@gmail.com>
2024-02-09 16:25:13 -0300

    ✨ feat: implements fetch nearby gyms service

--- src/repositories/gyms-repository.ts
+++ src/repositories/gyms-repository.ts
@@ -1,7 +1,13 @@
 import { Gym, Prisma } from '@prisma/client'
 
+export interface FindManyNearbyParams {
+  latitude: number
+  longitude: number
+}
+
 export interface GymsRepository {
   create(data: Prisma.GymCreateInput): Promise<Gym>
+  findManyNearby(params: FindManyNearbyParams): Promise<Gym[]>
   searchMany(query: string, page: number): Promise<Gym[]>
   findById(id: string): Promise<Gym | null>
 }
