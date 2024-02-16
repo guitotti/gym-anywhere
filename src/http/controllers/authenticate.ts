f953e2a7f0e96524311a9d51cc2cfa78c71b584b
Guilherme Totti <dev.guilhermetotti@gmail.com>
2024-02-16 15:52:03 -0300

    ✨ feat: implements JWT in fastify

--- src/http/controllers/authenticate.ts
+++ src/http/controllers/authenticate.ts
@@ -17,10 +17,21 @@ export async function authenticate(
   try {
     const authenticateService = makeAuthenticateService()
 
-    await authenticateService.execute({
+    const { user } = await authenticateService.execute({
       email,
       password,
     })
+
+    const token = await reply.jwtSign(
+      {},
+      {
+        sign: {
+          sub: user.id,
+        },
+      },
+    )
+
+    return reply.status(200).send(token)
   } catch (error) {
     if (error instanceof InvalidCredentialsError) {
       return reply.status(400).send({ message: error.message })
@@ -28,6 +39,4 @@ export async function authenticate(
 
     throw error
   }
-
-  return reply.status(200).send()
 }
