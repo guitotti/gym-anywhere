cd12b4b7d90c98ddc808dbcd6bcc7d12187152a8
Guilherme Totti <dev.guilhermetotti@gmail.com>
2024-02-20 10:36:30 -0300

    ✨ feat: create user profile controller

--- src/http/controllers/profile.ts
+++ src/http/controllers/profile.ts
@@ -1,9 +1,17 @@
+import { makeGetUserProfileService } from '@/services/factories/make-get-user-profile-service'
 import { FastifyReply, FastifyRequest } from 'fastify'
 
 export async function profile(request: FastifyRequest, reply: FastifyReply) {
-  await request.jwtVerify()
+  const getUserProfile = makeGetUserProfileService()
 
-  console.log(request.user.sub)
+  const { user } = await getUserProfile.execute({
+    userId: request.user.sub,
+  })
 
-  return reply.status(200).send()
+  return reply.status(200).send({
+    user: {
+      ...user,
+      password_hash: undefined,
+    },
+  })
 }
