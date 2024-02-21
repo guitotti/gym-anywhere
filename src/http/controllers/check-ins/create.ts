d2c4c467ba29c6a8ad50094d85824e5859d092cd
Guilherme Totti <dev.guilhermetotti@gmail.com>
2024-02-21 13:44:33 -0300

    ✨ feat: add check-ins controllers and routes

---
+++ src/http/controllers/check-ins/create.ts
@@ -0,0 +1,32 @@
+import { FastifyReply, FastifyRequest } from 'fastify'
+import { z } from 'zod'
+import { makeCheckInService } from '@/services/factories/make-check-ins-service'
+
+export async function create(request: FastifyRequest, reply: FastifyReply) {
+  const createCheckInParamsSchema = z.object({
+    gymId: z.string().uuid(),
+  })
+
+  const createCheckInBodySchema = z.object({
+    latitude: z.number().refine((value) => {
+      return Math.abs(value) <= 90
+    }),
+    longitude: z.number().refine((value) => {
+      return Math.abs(value) <= 180
+    }),
+  })
+
+  const { gymId } = createCheckInParamsSchema.parse(request.params)
+  const { latitude, longitude } = createCheckInBodySchema.parse(request.body)
+
+  const checkInService = makeCheckInService()
+
+  await checkInService.execute({
+    gymId,
+    userId: request.user.sub,
+    userLatitude: latitude,
+    userLongitude: longitude,
+  })
+
+  return reply.status(201).send()
+}
