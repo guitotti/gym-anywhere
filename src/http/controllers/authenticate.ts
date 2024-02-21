d224ccb89d5d7f4651e54652c0ebb458ba4d7b2e
Guilherme Totti <dev.guilhermetotti@gmail.com>
2024-02-21 11:54:20 -0300

    🧪 test: [E2E] add register and authenticate specs

--- src/http/controllers/authenticate.ts
+++ src/http/controllers/authenticate.ts
@@ -31,7 +31,9 @@ export async function authenticate(
       },
     )
 
-    return reply.status(200).send(token)
+    return reply.status(200).send({
+      token,
+    })
   } catch (error) {
     if (error instanceof InvalidCredentialsError) {
       return reply.status(400).send({ message: error.message })
