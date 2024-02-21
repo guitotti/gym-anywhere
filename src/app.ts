de3fe9d03e774b952f31e25a534c376f927277d4
Guilherme Totti <dev.guilhermetotti@gmail.com>
2024-02-21 17:22:45 -0300

    ✨ feat: implements refresh token and cookie

--- src/app.ts
+++ src/app.ts
@@ -7,13 +7,22 @@ import { checkInsRoutes } from './http/controllers/check-ins/routes'
 import { ZodError } from 'zod'
 import { env } from './env'
 import fastifyJwt from '@fastify/jwt'
+import fastifyCookie from '@fastify/cookie'
 
 export const app = fastify()
 
 app.register(fastifyJwt, {
   secret: env.JWT_SECRET,
+  cookie: {
+    cookieName: 'refreshToken',
+    signed: false,
+  },
+  sign: {
+    expiresIn: '10m',
+  },
 })
 
+app.register(fastifyCookie)
 app.register(usersRoutes)
 app.register(gymsRoutes)
 app.register(checkInsRoutes)
